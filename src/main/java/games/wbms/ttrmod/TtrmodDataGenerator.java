8e3184a004c18e5003fa55360155e7230b1c6b19
Xiao Han Ye <92127505+yuchen1204@users.noreply.github.com>
2024-09-24 22:51:28 +0800

    wbms-alpha-04 update

---
+++ src/main/java/games/wbms/ttrmod/TtrmodDataGenerator.java
@@ -0,0 +1,11 @@
+package games.wbms.ttrmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TtrmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/games/wbms/ttrmod/ttrmodclient.java
@@ -0,0 +1,10 @@
+package games.wbms.ttrmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ttrmodclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
