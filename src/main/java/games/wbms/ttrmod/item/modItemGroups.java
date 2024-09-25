4ebedf39d159c7dce6e350e7689bcc4e5b0809d7
Xiao Han Ye <92127505+yuchen1204@users.noreply.github.com>
2024-09-25 20:48:10 +0800

    wbms-alpha-04 update

--- src/main/java/games/wbms/ttrmod/item/modItemGroups.java
+++ src/main/java/games/wbms/ttrmod/item/modItemGroups.java
@@ -2,16 +2,23 @@ package games.wbms.ttrmod.item;
 
 import games.wbms.ttrmod.ttrmod;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class modItemGroups {
-    public static final RegistryKey<ItemGroup> ttr_Group = register("ttr_Group");
+    public static final ItemGroup TTRMOD = Registry.register(Registries.ITEM_GROUP,Identifier.of(ttrmod.MOD_ID,"ttrmod"),
+            ItemGroup.create(null,-1).displayName(Text.translatable("itemGroup.ttrmod"))
+                    .icon(() -> new ItemStack(ModItems.ICE_ETHER))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModItems.ICE_ETHER);
+                    }).build());
 
-
-    private static RegistryKey<ItemGroup> register(String id) {
-        return RegistryKey.of(RegistryKeys.ITEM_GROUP, Identifier.of(ttrmod.MOD_ID,id));
+    public static void registerModItemGroups() {
+        ttrmod.LOGGER.info("Registering Mod Item Groups");
     }
-
-}
+    }
